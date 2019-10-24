ad4e136a4eb919bfe2b9937ab1bf81411195cd9f
LordPompax <capitaine_ted@hotmail.fr>
2019-10-24 21:58:19 +0200

    autre maj

---
+++ public/case.js
@@ -0,0 +1,28 @@
+class Case {
+	constructor(ctx, mapx, mapy, nbx, nby, t, mine) {
+		this.ctx = ctx
+		this.nbx = nbx
+		this.nby = nby
+		this.mapx = mapx
+		this.mapy = mapy
+		this.t = t
+		this.mine = mine
+	}
+
+	get getX() { return this.nbx }
+	get getY() { return this.nby }
+
+	drawCase() {
+		const x = this.mapx + this.t * this.nbx
+		const y = this.mapy + this.t * this.nby
+		this.ctx.strokeStyle = '#000000'
+		this.ctx.strokeRect(x, y, this.t, this.t)
+	}
+
+	eventCase(canvas) {
+		canvas.addEventListenner((e) => {
+			const x = e.clientX(), y = e.clientY()
+			
+		})
+	}
+}
