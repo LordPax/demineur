4456b567458d6fef2aafeb52eaaa24428e1fe612
LordPompax <capitaine_ted@hotmail.fr>
2019-11-12 00:18:54 +0100

    maj dernier commit

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,12 @@ const t_case:number = 40
 const mapx:number = width / 2 - (lx * t_case) / 2
 const mapy:number = height / 2 - (ly * t_case) / 2
 
-const superfor = (compt:number, f:any, acc:number = 0, r:any = null):any => {
+interface verifCase {
+    x:number,
+    y:number
+}
+
+const superfor = (compt:number, f:(number, any) => any, acc:number = 0, r:any = null):any => {
     const re:any = acc < compt ? f(acc, r) : r
     return acc < compt ? superfor(compt, f, acc + 1, re) : re
 }
@@ -23,7 +28,7 @@ const superfor = (compt:number, f:any, acc:number = 0, r:any = null):any => {
 const rand = (min:number, max:number):number => Math.floor(Math.random() * (max - min + 1) + min)
 const aleaMine = n => rand(0, n) === 0 ? 1 : 0
 
-const vCase:any[] = [
+const vCase:verifCase[] = [
     {x : 1, y : 0},
     {x : 1, y : 1},
     {x : 0, y : 1},
